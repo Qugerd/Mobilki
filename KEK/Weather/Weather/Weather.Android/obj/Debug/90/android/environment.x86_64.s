b1c831717d92f0344d8ca379677d13af521c3f55
Qugerd <panchenko.nk@students.dvfu.ru>
2021-01-18 00:20:27 +0600

    oo

--- KEK/Weather/Weather/Weather.Android/obj/Debug/90/android/environment.x86_64.s
+++ KEK/Weather/Weather/Weather.Android/obj/Debug/90/android/environment.x86_64.s
@@ -72,7 +72,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"de204218-b420-4701-9919-3e1c19620baf"
+	.asciz	"ac8268bb-bfce-4b48-a4f2-c514712d8f37"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
